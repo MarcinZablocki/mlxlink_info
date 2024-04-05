af1c748a74f5df227db198e80f9a5aae4f363231
Marcin Zablocki <marcin.zablocki@oracle.com>
2024-04-05 09:20:39 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,275 @@
+#![allow(dead_code)]
+use std::fs;
+use std::process;
+use tabled::{
+    settings::{peaker::PriorityMax, Height, Settings, Width, Style},
+    Table, Tabled
+};
+use clap::Parser;
+//use log::{info, warn, debug, error};
+use terminal_size::{terminal_size, Height as TerminalHeight, Width as TerminalWidth};
+use natord::compare;
+use std::sync::mpsc;
+use std::thread;
+
+use gethostname::gethostname;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Display errors only
+    #[arg(short, long, default_value_t = false)]
+    errors: bool,
+}
+
+fn get_terminal_size() -> (usize, usize) {
+    let (TerminalWidth(width), TerminalHeight(height)) =
+        terminal_size().expect("failed to obtain a terminal size");
+
+    (width as usize, height as usize)
+}
+
+fn parse_result(device : Mlx5Port, _data: json::JsonValue, host_serial: String, host_name: String) -> Mlx5PortStats {
+    let mut _mlx5_port_stats = Mlx5PortStats {
+        raw_physical_errors_per_lane_0: _data["Physical Counters and BER Info"]["Raw Physical Errors Per Lane"]["values"][0].to_string().parse::<u64>().unwrap(),
+        raw_physical_errors_per_lane_1: _data["Physical Counters and BER Info"]["Raw Physical Errors Per Lane"]["values"][1].to_string().parse::<u64>().unwrap(),
+        raw_physical_errors_per_lane_2: _data["Physical Counters and BER Info"]["Raw Physical Errors Per Lane"]["values"][2].to_string().parse::<u64>().unwrap(),
+        raw_physical_errors_per_lane_3: _data["Physical Counters and BER Info"]["Raw Physical Errors Per Lane"]["values"][3].to_string().parse::<u64>().unwrap(),
+        effective_physical_errors: _data["Physical Counters and BER Info"]["Effective Physical Errors"].to_string().parse::<u64>().unwrap(),
+        effective_physical_ber: _data["Physical Counters and BER Info"]["Effective Physical BER"].as_str().unwrap().to_string(),
+        raw_physical_ber: _data["Physical Counters and BER Info"]["Raw Physical BER"].as_str().unwrap().to_string(),
+        vendor_serial: _data["Module Info"]["Vendor Serial Number"].as_str().unwrap().to_string(),
+        recommedation: _data["Troubleshooting Info"]["Recommendation"].as_str().unwrap().to_string(),
+        link_state: _data["Operational Info"]["State"].as_str().unwrap().to_string(),
+        fec_bin_0: _data["Histogram of FEC Errors"]["Bin 0"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_1: _data["Histogram of FEC Errors"]["Bin 1"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_2: _data["Histogram of FEC Errors"]["Bin 2"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_3: _data["Histogram of FEC Errors"]["Bin 3"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_4: _data["Histogram of FEC Errors"]["Bin 4"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_5: _data["Histogram of FEC Errors"]["Bin 5"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_6: _data["Histogram of FEC Errors"]["Bin 6"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_7: _data["Histogram of FEC Errors"]["Bin 7"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_8: _data["Histogram of FEC Errors"]["Bin 8"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_9: _data["Histogram of FEC Errors"]["Bin 9"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_10: _data["Histogram of FEC Errors"]["Bin 10"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_11: _data["Histogram of FEC Errors"]["Bin 11"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_12: _data["Histogram of FEC Errors"]["Bin 12"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_13: _data["Histogram of FEC Errors"]["Bin 13"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_14: _data["Histogram of FEC Errors"]["Bin 14"]["values"][1].to_string().parse::<u64>().unwrap(),
+        fec_bin_15: _data["Histogram of FEC Errors"]["Bin 15"]["values"][1].to_string().parse::<u64>().unwrap(),
+        device: device,
+        comments: "".to_string(),
+        host_serial: host_serial,
+        hostname: host_name,
+        error: false,
+};
+
+    if _mlx5_port_stats.link_state == "Active" {
+        _mlx5_port_stats.link_state = "✅ Active".to_string();
+    } else {
+        _mlx5_port_stats.link_state = format!("❌ {}",_mlx5_port_stats.link_state).to_string();
+        _mlx5_port_stats.comments = "Link is down".to_string();
+    }
+
+    if _mlx5_port_stats.effective_physical_ber.parse::<f64>().unwrap() > 0.0000000010 {
+        _mlx5_port_stats.comments = "❌ Physical BER is high".to_string();
+    }
+
+    if _mlx5_port_stats.effective_physical_errors > 0 {
+        _mlx5_port_stats.comments = "❌ Physical errors detected".to_string();
+    }
+
+    return _mlx5_port_stats;
+}
+
+
+#[derive(Tabled)]
+#[derive(Clone)]
+struct Mlx5Port {
+    device: String,
+    port: String,
+}
+
+#[derive(Tabled)]
+#[derive(Clone)]
+struct Mlx5PortStats {
+    host_serial: String,
+    hostname: String,
+    #[tabled(inline)]
+    device: Mlx5Port,
+    #[tabled(rename = "bin0")]
+    fec_bin_0: u64,
+    #[tabled(rename = "bin1")]
+    fec_bin_1: u64,
+    #[tabled(skip)]
+    #[tabled(rename = "bin2")]
+    fec_bin_2: u64,
+    #[tabled(rename = "bin3")]
+    #[tabled(skip)]
+    fec_bin_3: u64,
+    #[tabled(rename = "bin4")]
+    #[tabled(skip)]
+    fec_bin_4: u64,
+    #[tabled(rename = "bin5")]
+    #[tabled(skip)]
+    fec_bin_5: u64,
+    #[tabled(rename = "bin6")]
+    fec_bin_6: u64,
+    #[tabled(rename = "bin7")]
+    fec_bin_7: u64,
+    #[tabled(rename = "bin8")]
+    fec_bin_8: u64,
+    #[tabled(rename = "bin8")]
+    fec_bin_9: u64,
+    #[tabled(rename = "bin9")]
+    fec_bin_10: u64,
+    #[tabled(rename = "bin10")]
+    fec_bin_11: u64,
+    #[tabled(rename = "bin11")]
+    fec_bin_12: u64,
+    #[tabled(rename = "bin12")]
+    fec_bin_13: u64,
+    #[tabled(rename = "bin13")]
+    fec_bin_14: u64,
+    #[tabled(rename = "bin14")]
+    fec_bin_15: u64,
+    #[tabled(rename = "bin15")]
+    #[tabled(rename = "raw err 0 ")]
+    raw_physical_errors_per_lane_0: u64,
+    #[tabled(rename = "raw err 1 ")]
+    raw_physical_errors_per_lane_1: u64,
+    #[tabled(rename = "raw err 2 ")]
+    raw_physical_errors_per_lane_2: u64,
+    #[tabled(rename = "raw err 3 ")]
+    raw_physical_errors_per_lane_3: u64,
+    link_state: String,
+    vendor_serial: String,
+    #[tabled(rename = "phys err")]
+    effective_physical_errors: u64,
+    #[tabled(rename = "phys ber")]
+    effective_physical_ber: String,
+    #[tabled(rename = "raw ber")]
+    raw_physical_ber: String,
+    recommedation: String,
+    comments: String, 
+    #[tabled(skip)]
+    error: bool,
+
+}
+
+
+fn collect_mlxlink_output(device : &Mlx5Port) -> json::JsonValue {
+    let command = format!("mlxlink -m -e -c -d {} --rx_fec_histogram --show_histogram --json", device.port);
+    let output = process::Command::new("sh")
+        .arg("-c")
+        .arg(&command)
+        .output()
+        .expect("failed to execute process");
+
+    let json_data = json::parse(String::from_utf8_lossy(&output.stdout).as_ref()).unwrap();
+    
+
+    let _data = json_data["result"]["output"].clone();
+    //println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    //println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    return _data;
+}
+
+fn print_table(all_port_stats: Vec<Mlx5PortStats>) {
+    let mut all_port_stats = all_port_stats;
+    let (width, height) = get_terminal_size();
+    all_port_stats.sort_by(|a, b| compare(&a.device.port, &b.device.port));
+    let table_settings = Settings::default()
+    .with(Width::wrap(width).priority::<PriorityMax>())
+    .with(Width::truncate(width))
+    .with(Style::modern_rounded());
+
+    //    .with(Height::limit(height))
+
+
+    let mut table = Table::new(all_port_stats);
+    table.with(table_settings);
+    
+    println!("{}", table);
+}
+
+fn get_mlx5_ports() -> Vec<Mlx5Port> {
+    let _rdma_ports_list = fs::read_dir("/sys/class/net")
+        .unwrap()
+        .map(|entry| entry.unwrap().file_name().into_string().unwrap())
+        .filter(|device| device.starts_with("rdma"))
+        .map(|device| Mlx5Port {
+            device: device.clone(),
+            port: fs::read_dir(format!("/sys/class/net/{}/device/infiniband", device))
+                .unwrap()
+                .map(|entry| entry.unwrap().file_name().into_string().unwrap())
+                .collect::<Vec<String>>()
+                .join(","),
+        })
+        .collect::<Vec<Mlx5Port>>();
+
+    return _rdma_ports_list;
+}
+
+fn check_root() {
+    #[link(name = "c")]
+    extern "C" {
+        fn geteuid() -> u32;
+        fn getegid() -> u32;
+    }
+
+    let _timeout = 60;
+
+    let _euid = unsafe { geteuid() };
+    let _egid = unsafe { getegid() };
+
+    if _euid != 0 {
+        println!("You must be root to run this program.");
+        std::process::exit(1);
+    }
+}
+
+fn main() {
+    
+    check_root();
+
+    let args = Args::parse();
+
+    let _rdma_ports_list = get_mlx5_ports();
+    let rdma_ports_list = _rdma_ports_list.clone();
+    let mut _chassis_serial = fs::read_to_string("/sys/class/dmi/id/chassis_serial").unwrap();
+    let mut _hostname = gethostname().to_string_lossy().to_string();
+
+    let mut all_port_stats = Vec::<Mlx5PortStats>::new();
+
+    // For mlx5 device in _rdma_ports_list use threads to collect data
+    let (tx, rx) = mpsc::channel();
+    for _mlx5_device in _rdma_ports_list {
+        let tx = tx.clone();
+        let _chassis_serial = _chassis_serial.clone();
+
+        let _hostname = _hostname.clone();
+        thread::spawn(move || {
+            let _output = collect_mlxlink_output(&_mlx5_device);
+            let mut _mlx5_port_stats = parse_result(_mlx5_device, _output, _chassis_serial, _hostname.clone());
+            tx.send(_mlx5_port_stats).unwrap();
+        });
+    }
+
+    for _ in rdma_ports_list {
+        let _mlx5_port_stats = rx.recv().unwrap();
+        all_port_stats.push(_mlx5_port_stats);
+    }
+
+
+    // for _mlx5_device in _rdma_ports_list {
+    //     let _output = collect_mlxlink_output(&_mlx5_device);
+    //     let mut _mlx5_port_stats = parse_result(_mlx5_device, _output, _chassis_serial.clone(), _hostname.clone());
+    //     all_port_stats.push(_mlx5_port_stats);
+
+        
+    // }
+
+    print_table(all_port_stats);
+}
